40eab0dcf03c244ce4a894e2263ad617c48004a6
gumanist29 <rrahmanjanov@gmail.com>
2019-03-04 09:05:30 +0600

    done

--- mp3con/models.py
+++ mp3con/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here
 class Utuber(models.Model):
     name=models.CharField(max_length=130)
-    pub_data=models.DateTimeField()
 
     def __str__(self):
         return self.name
