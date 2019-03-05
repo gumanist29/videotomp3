1d1896c3dd7621f6952d8ddff3daa04db0f36743
gumanist29 <rrahmanjanov@gmail.com>
2019-03-05 10:18:52 +0600

    done

--- mp3con/views.py
+++ mp3con/views.py
@@ -8,15 +8,18 @@ from .redis import download_mp3
 class HomeView(generic.TemplateView):
     def get(self, request):
         return render(request,'index.html')
+
     def post(self, request):
         urls = request.POST.get('url')
         if urls!=200:
             return HttpResponse("Not right link")
         else:
             download_mp3(urls)
-            new_utuber = Utuber(name = urls)
+            new_utuber = Utuber(name=urls)
             new_utuber.save()
             return HttpResponse("Downloaded check in your Folder")
+
+
 
 def stories(request):
     all_link_list = Utuber.objects.all()
